e21355cfc643ee07fa43147787509f12f423e044
senseiwells <wellsm488@gmail.com>
2024-02-17 17:10:38 +0000

    Fix uhc pack being sent all the time

--- src/main/kotlin/net/casual/championships/resources/CasualResourcePack.kt
+++ src/main/kotlin/net/casual/championships/resources/CasualResourcePack.kt
@@ -3,6 +3,7 @@ package net.casual.championships.resources
 import net.casual.arcade.resources.ArcadePacks
 import net.casual.arcade.resources.NamedResourcePackCreator
 import net.casual.arcade.utils.ComponentUtils.literal
+import net.casual.arcade.utils.impl.ConcatenatedList.Companion.concat
 import net.casual.championships.CasualMod
 import net.casual.championships.common.CasualCommonMod
 import net.casual.championships.common.item.CasualCommonItems
@@ -20,18 +21,19 @@ object CasualResourcePack {
         packIcon = path.readBytes()
     }
 
-    private val packs = listOf(
+    private val default = listOf(
         ArcadePacks.NO_SHADOW_PACK,
         ArcadePacks.SPACES_FONT_PACK,
         ArcadePacks.ACTION_BAR_FONT_PACK,
         CasualCommonMod.COMMON_PACK,
         CasualCommonItems.CUSTOM_MODEL_PACK,
-        UHCMod.UHC_PACK,
         CHAMPIONSHIPS_PACK
     )
 
+    private val all = default.concat(listOf(UHCMod.UHC_PACK))
+
     fun generate() {
-        for (pack in this.packs) {
+        for (pack in this.all) {
             try {
                 pack.buildTo(Config.resolve("packs"))
             } catch (e: Exception) {
@@ -41,6 +43,6 @@ object CasualResourcePack {
     }
 
     fun getPackNames(): List<String> {
-        return this.packs.map { it.zippedName() }
+        return this.default.map { it.zippedName() }
     }
 }
