0d9233d227446c7ef200b517e1a1842ebc8de5b3
senseiwells <wellsm488@gmail.com>
2025-08-01 00:55:41 +0100

    chore: Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 allprojects {
     group = "net.casual"
-    version = "1.1.27"
+    version = "1.2.0"
 
     apply(plugin = "fabric-loom")
     apply(plugin = "org.jetbrains.kotlin.jvm")
