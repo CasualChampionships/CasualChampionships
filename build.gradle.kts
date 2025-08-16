f2f470c9e23d9dad5a594fbd56cacbeb3fd2e693
senseiwells <wellsm488@gmail.com>
2025-08-16 11:18:15 +0100

    feat: Add lithium options to fabric.mod.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 allprojects {
     group = "net.casual"
-    version = "1.3.1"
+    version = "1.4.0"
 
     apply(plugin = "fabric-loom")
     apply(plugin = "org.jetbrains.kotlin.jvm")
