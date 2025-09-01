535d0365bfe332b0c9aa53ed131a272883a8d714
senseiwells <wellsm488@gmail.com>
2025-09-01 19:18:29 +0100

    fix: Boundary not serializing correctly

--- minigames-uhc/src/main/kotlin/net/casual/championships/uhc/border/UHCBoundaryManager.kt
+++ minigames-uhc/src/main/kotlin/net/casual/championships/uhc/border/UHCBoundaryManager.kt
@@ -77,9 +77,11 @@ object UHCBoundaryManager {
 
         uhc.onPauseBoundary()
         val cooldown = current.getCooldown(uhc.settings.borderTime)
+        val index = UHCBoundaryPhase.entries.indexOf(current)
+        require(index >= 0) { "Boundary phase was not registered!?" }
         uhc.scheduler.schedulePhased(cooldown, MinigameTask(uhc) { minigame ->
             minigame.onResumeBoundary()
-            this.move(minigame, current.getNextStage())
+            this.move(minigame, UHCBoundaryPhase.entries[index].getNextStage())
         })
     }
 
